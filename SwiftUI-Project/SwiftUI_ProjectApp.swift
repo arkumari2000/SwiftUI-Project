c16dd8f9afeff7e783eb029cfd81a9a134963e86
Archana Kumari <arkumari2000@gmail.com>
2025-07-18 20:06:39 +0530

    Initial Commit

---
+++ SwiftUI-Project/SwiftUI_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ProjectApp.swift
+//  SwiftUI-Project
+//
+//  Created by Himanshu Bhati on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-ProjectTests/SwiftUI_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ProjectTests.swift
+//  SwiftUI-ProjectTests
+//
+//  Created by Himanshu Bhati on 18/07/25.
+//
+
+import Testing
+@testable import SwiftUI_Project
+
+struct SwiftUI_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
